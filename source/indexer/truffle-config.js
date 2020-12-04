90dbee5b847b9a0ee5e883f348c8b49298e072c2
Ethan Wessel <ejwessel@gmail.com>
2020-12-04 13:35:50 -0800

    ignore gas price in config

--- source/indexer/truffle-config.js
+++ source/indexer/truffle-config.js
@@ -26,7 +26,7 @@ module.exports = {
           'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY
         ),
       // gas: 1098551,
-      gasPrice: 39e9,
+      // gasPrice: 40e9,
       network_id: 1,
     },
     rinkeby: {
